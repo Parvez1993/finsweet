ffd5bd8696eba82bd1f2f947f4d7467d94cb1dfa
Parvez1993 <mohammedparvez1923@gmail.com>
2022-09-01 18:16:42 +0600

    Banner Service Page

---
+++ src/Components/Service/Service.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+function Service() {
+  return (
+    <section class="services">
+      <div class="container">
+        <div class="box">
+          <div class="left">
+            <h2>How we work</h2>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor.
+            </p>
+            <a class="sercices__contact">
+              Get in touch with us
+              <i class="fa-solid fa-arrow-right-long"> </i>
+            </a>
+          </div>
+          <div class="right">
+            <div class="right__content">
+              <img src="images/no1.png" alt="1" />
+              <h2>Strategy</h2>
+              <p>
+                Euismod faucibus turpis eu gravida mi. Pellentesque et velit
+                aliquam .
+              </p>
+            </div>
+            <div class="right__content">
+              <img src="images/no2.png" alt="1" />
+              <h2>Wireframing</h2>
+              <p>
+                Euismod faucibus turpis eu gravida mi. Pellentesque et velit
+                aliquam .
+              </p>
+            </div>
+            <div class="right__content">
+              <img src="images/no3.png" alt="1" />
+              <h2>Design</h2>
+              <p>
+                Euismod faucibus turpis eu gravida mi. Pellentesque et velit
+                aliquam .
+              </p>
+            </div>
+            <div class="right__content">
+              <img src="images/no4.png" alt="1" />
+              <h2>Development</h2>
+              <p>
+                Euismod faucibus turpis eu gravida mi. Pellentesque et velit
+                aliquam .
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Service;
--- src/Pages/Homepage.js
+++ src/Pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from "react";
+import Banner from "../Components/Banner/Banner";
 import Nav from "../Components/Nav/Nav";
 
 function Homepage() {
   return (
     <>
       <Nav />
+      <Banner />
     </>
   );
 }
